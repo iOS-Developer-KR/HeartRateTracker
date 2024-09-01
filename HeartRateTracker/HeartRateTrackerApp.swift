542b26062f29285c467d5158a3634402f0761c26
happyytw <happyytw@gmail.com>
2024-09-02 01:00:56 +0900

    Initial Commit

---
+++ HeartRateTracker/HeartRateTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateTrackerApp.swift
+//  HeartRateTracker
+//
+//  Created by Taewon Yoon on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
